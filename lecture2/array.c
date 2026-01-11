ab2bd16277c59c5209933f63628375d0822027e4
h108777 <harshiluttaradhi18@gmail.com>
2026-01-11 06:16:36 +0530

    learning arrays

---
+++ lecture2/array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int marks[3]={10,20,30}; //array intialization and assigning values 
+	for(int i=0;i<3;i++){
+
+		printf("%d\n",marks[i]);
+					}
+	return 0;
+			}
+
+	
