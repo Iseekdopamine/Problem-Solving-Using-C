5f31476a582e52c16fa48c80f5798065678aa77e
h108777 <lifeisjustahardkickinurethra@gmail.com>
2026-01-16 13:29:21 +0530

    understood how ternary operator in c works

---
+++ lecture3+4/conditionaloperator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int var;
+	int flag;
+	printf("Enter the value of flag:");
+	scanf("%d",&flag);
+	var=(flag==0)?16:-16;
+	printf("Value of var is %d", var);
+}
+
+	
